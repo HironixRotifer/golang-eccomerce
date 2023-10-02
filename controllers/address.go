b3aca282ce03832517a2474a8db7dbe1af3e13bc
vadbmak23 <vadbmak23@gmail.com>
2023-10-02 10:38:49 +0400

    update address.go and cart.go

--- controllers/address.go
+++ controllers/address.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -9,23 +10,141 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
+// AddAddress add new address from user id
 func AddAddress() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		user_id := c.Query("userID")
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"erorr": "Invalid code"})
+			c.Abort()
+			return
+		}
 
+		address, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, "Internal Server Error")
+		}
+
+		var addresses models.Address
+
+		addresses.Address_id = primitive.NewObjectID()
+
+		if err := c.BindJSON(&addresses); err != nil {
+			c.IndentedJSON(http.StatusNotAcceptable, err.Error())
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		match_filter := bson.D{{Key: "$math", Value: bson.D{primitive.E{Key: "_id", Value: address}}}}
+		unwind := bson.D{{Key: "$unwind", Value: bson.D{primitive.E{Key: "path", Value: "$address"}}}}
+		grouping := bson.D{{Key: "$group", Value: bson.D{primitive.E{Key: "_id", Value: "$address_id"}, {Key: "count", Value: bson.D{primitive.E{Key: "$sum", Value: 1}}}}}}
+
+		pointcursor, err := UserCollection.Aggregate(ctx, mongo.Pipeline{match_filter, unwind, grouping})
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, "Internal Server Error")
+		}
+
+		var addressinfo []bson.M
+		if err := pointcursor.All(ctx, &addressinfo); err != nil {
+			panic(err)
+		}
+
+		var size int32
+		for _, address_no := range addressinfo {
+			count := address_no["count"]
+			size = count.(int32)
+		}
+
+		if size < 2 {
+			filter := bson.D{primitive.E{Key: "_id", Value: address}}
+			update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "address", Value: addresses}}}}
+			_, err := UserCollection.UpdateOne(ctx, filter, update)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			c.IndentedJSON(http.StatusNotFound, "Not Allowed")
+		}
+
+		defer cancel()
+		ctx.Done()
 	}
 }
 
+// EditHomeAddress update home address from user id
 func EditHomeAddress() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		user_id := c.Query("userID")
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"erorr": "Invalid code"})
+			c.Abort()
+			return
+		}
 
+		userObjectID, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, "Internal Server error")
+		}
+
+		var editaddress models.Address
+		if err := c.BindJSON(editaddress); err != nil {
+			c.IndentedJSON(http.StatusBadRequest, err.Error())
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		filter := bson.D{primitive.E{Key: "_id", Value: userObjectID}}
+		update := bson.D{{Key: "$set", Value: bson.D{primitive.E{Key: "address.0.house_name", Value: editaddress.House}, {Key: "address.0.street_name", Value: editaddress.Street}, {Key: "address.0.city_name", Value: editaddress.City}, {Key: "address.0.pin_code", Value: editaddress.Pincode}}}}
+		_, err = UserCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, "Something went wrong")
+			return
+		}
+
+		defer cancel()
+		ctx.Done()
+		c.IndentedJSON(http.StatusOK, "Successfully update the home address")
 	}
 }
 
+// EditWorkAddress update work address from user id
 func EditWorkAddress() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		user_id := c.Query("userID")
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"erorr": "Invalid code"})
+			c.Abort()
+			return
+		}
 
+		userObjectID, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, "Internal Server error")
+		}
+
+		var editaddress models.Address
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		filter := bson.D{primitive.E{Key: "_id", Value: userObjectID}}
+		update := bson.D{{Key: "$set", Value: bson.D{primitive.E{Key: "address.1.house_name", Value: editaddress.House}, {Key: "address.1.street_name", Value: editaddress.Street}, {Key: "address.1.city_name", Value: editaddress.City}, {Key: "address.1.pin_code", Value: editaddress.Pincode}}}}
+		_, err = UserCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, "Something went wrong")
+			return
+		}
+
+		defer cancel()
+		ctx.Done()
+		c.IndentedJSON(http.StatusOK, "Successfully update the work address")
 	}
 }
 
--- controllers/cart.go
+++ controllers/cart.go
@@ -8,8 +8,10 @@ import (
 	"time"
 
 	"github.com/HironixRotifer/golang-eccomerce/database"
+	"github.com/HironixRotifer/golang-eccomerce/models"
 	"github.com/gin-gonic/gin"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -99,8 +101,50 @@ func (app *Application) RemoveItem() gin.HandlerFunc {
 }
 
 func (app *Application) GetItemFromCart() gin.HandlerFunc {
+
 	return func(c *gin.Context) {
 
+		user_id := c.Query("userID")
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"error": "invalid id"})
+			c.Abort()
+			return
+		}
+		usert_id, _ := primitive.ObjectIDFromHex(user_id)
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var filledcart models.User
+		err := UserCollection.FindOne(ctx, bson.D{primitive.E{Key: "_id", Value: usert_id}}).Decode(&filledcart)
+		if err != nil {
+			log.Println(err)
+			c.IndentedJSON(http.StatusInternalServerError, "not found")
+			return
+		}
+
+		filter_match := bson.D{{Key: "$match", Value: bson.D{primitive.E{Key: "_id", Value: usert_id}}}}
+		unwind := bson.D{{Key: "$unwind", Value: bson.D{primitive.E{Key: "path", Value: "$usercart"}}}}
+		grouping := bson.D{{Key: "$group", Value: bson.D{primitive.E{Key: "_id", Value: "_id"}, {Key: "total", Value: bson.E{Key: "$sum", Value: "$usercart.price"}}}}}
+
+		pointcursor, err := UserCollection.Aggregate(ctx, mongo.Pipeline{filter_match, unwind, grouping})
+		if err != nil {
+			log.Println(err)
+		}
+
+		var listing []bson.M
+		if err := pointcursor.All(ctx, &listing); err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+
+		for _, json := range listing {
+			c.IndentedJSON(http.StatusOK, json["total"])
+			c.IndentedJSON(http.StatusOK, filledcart.User_Cart)
+		}
+
+		ctx.Done()
 	}
 }
 
