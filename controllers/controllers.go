47f59249fd0b07e57e43f2473332685a9c44a4d9
vadbmak23 <vadbmak23@gmail.com>
2023-10-02 12:36:43 +0400

    update database

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/HironixRotifer/golang-eccomerce/database"
 	"github.com/HironixRotifer/golang-eccomerce/models"
+	"github.com/HironixRotifer/golang-eccomerce/tokens"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson"
@@ -43,7 +44,7 @@ func VerefiPassword(userPassword string, givenPassword string) (bool, string) {
 	return valid, msg
 }
 
-func Signup() gin.HandlerFunc {
+func SingUp() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		defer cancel()
